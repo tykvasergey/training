088714b2886663e59ef1d2fc89dca62b7f982ef9
Artem Popenko <artem.popenko@gmail.com>
2019-04-18 19:25:25 +0300

    1.2

---
+++ Training/TestOM/Model/Manager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\TestOM\Model;
+
+
+class Manager implements ManagerInterface
+{
+    public function create()
+    {}
+
+    public function get()
+    {}
+
+}
---
+++ Training/TestOM/Model/ManagerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Training\TestOM\Model;
+
+
+interface ManagerInterface
+{
+    public  function create();
+    public  function get();
+
+
+}
