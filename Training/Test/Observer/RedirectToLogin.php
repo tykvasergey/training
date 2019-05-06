56be2e5da1637e7738413a093b764e9a1d3b7e29
Artem Popenko <artem.popenko@gmail.com>
2019-05-06 23:04:31 +0300

    fixes of 1.1 - 1.10

--- Training/Test/Observer/RedirectToLogin.php
+++ Training/Test/Observer/RedirectToLogin.php
@@ -14,29 +14,34 @@ class RedirectToLogin implements ObserverInterface
      * @var \Magento\Framework\App\ActionFlag
      */
     protected $_actionFlag;
+
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $customerSession;
+
     /**
      * @param \Magento\Framework\App\Response\RedirectInterface $redirect
      * @param \Magento\Framework\App\ActionFlag $actionFlag
      */
     public function __construct(
         \Magento\Framework\App\Response\RedirectInterface $redirect,
-        \Magento\Framework\App\ActionFlag $actionFlag
+        \Magento\Framework\App\ActionFlag $actionFlag,
+        \Magento\Customer\Model\Session $customerSession
     ) {
         $this->_redirect = $redirect;
         $this->_actionFlag = $actionFlag;
+        $this->customerSession = $customerSession;
     }
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $request = $observer->getEvent()->getData('request');
-        if ($request->getModuleName() == 'catalog'
-            && $request->getControllerName() == 'product'
-            && $request->getActionName() == 'view'
-        ) {
-        // if ($request->getFullActionName() == 'catalog_product_view') { // altenative way
+         if ($request->getFullActionName() == 'catalog_product_view' &&
+             !$this->customerSession->isLoggedIn()
+         ) {
             $controller = $observer->getEvent()->getData('controller_action');
             $this->_actionFlag->set('', \Magento\Framework\App\Action\Action::FLAG_NO_DISPATCH, true);
-            $this->_redirect->redirect($controller->getResponse(), 'customer/account/login');
-
+            return $this->_redirect->redirect($controller->getResponse(), 'customer/account/login');
         }
     }
 }
