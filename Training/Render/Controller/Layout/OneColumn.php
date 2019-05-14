e201e722e979d77ff7ba45cde72858c9ecddb03a
Artem Popenko <artem.popenko@gmail.com>
2019-05-14 22:57:37 +0300

    3.10

---
+++ Training/Render/Controller/Layout/OneColumn.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Training\Render\Controller\Layout;
+
+
+class OneColumn extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
