ffd674933f4ae97b9dc609b8640b5b0e898e1caf
Artem Popenko <artem.popenko@gmail.com>
2019-05-23 18:53:26 +0300

    4.3

---
+++ code/Training/Js/view/frontend/web/js/checkout/action/place-order-mixin.js
@@ -0,0 +1,18 @@
+define([
+        'underscore',
+        'jquery',
+        'mage/utils/wrapper'
+    ],function (_,$,wrapper) {
+        'use strict';
+
+        return function(placeOrderAction){
+            return wrapper.wrap(placeOrderAction, function (originalAction,paymentData,messageContainer) {
+                var additionalData = [];
+                additionalData['additional_data']=[];
+                additionalData['additional_data']['printed-ivoice']=$('#printed-invoice').prop('checked');
+                paymentData = _.extend(paymentData,additionalData);
+                return originalAction(paymentData,messageContainer);
+            })
+        }
+    }
+)
