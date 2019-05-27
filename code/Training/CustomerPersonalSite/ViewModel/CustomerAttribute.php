a35592aee828dc31eff44605aa5439b1447d59c1
Artem Popenko <artem.popenko@gmail.com>
2019-05-28 01:23:51 +0300

    5.3

---
+++ code/Training/CustomerPersonalSite/ViewModel/CustomerAttribute.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Training\CustomerPersonalSite\ViewModel;
+
+
+class CustomerAttribute implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    public function getPersonalSite($customerData)
+    {
+        $attribute = $customerData->getCustomAttribute('personal_site');
+        if ($attribute) {
+            return $attribute->getValue();
+        }
+        return '';
+    }
+}
